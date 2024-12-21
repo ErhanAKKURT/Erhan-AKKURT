91de0aa3bbd0b729ef59393780b72841c37809a6
Erhan AKKURT <erhanakkrt@gmail.com>
2024-12-21 21:37:07 +0300

    Create Creating a Strong Password.py

---
+++ pages/Creating a Strong Password.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import random
+import string
+st.title("Şifre Belirleme Uygulaması")
+
+kh = string.ascii_lowercase
+bh = string.ascii_uppercase
+rk = string.digits
+sy = string.punctuation
+
+khsec =random.choices(kh, k=2)
+bhsec =random.choices(bh, k=2)
+rksec = random.choices(rk, k=2)
+sysec =random.choices(sy, k=2)
+sifre = khsec+bhsec+rksec+sysec
+random.shuffle(sifre)
+sifre = "".join(sifre)
+st.write("Şifre Üretmek İçin Butona Tıklayınız.")
+btn = st.button("Şifre")
+if btn:
+    st.write(sifre)
+
+kod='''
+import streamlit as st
+import random
+import string
+st.title("Şifre Belirleme Uygulaması")
+
+kh = string.ascii_lowercase
+bh = string.ascii_uppercase
+rk = string.digits
+sy = string.punctuation
+
+khsec =random.choices(kh, k=2)
+bhsec =random.choices(bh, k=2)
+rksec = random.choices(rk, k=2)
+sysec =random.choices(sy, k=2)
+sifre = khsec+bhsec+rksec+sysec
+random.shuffle(sifre)
+sifre = "".join(sifre)
+st.write("Şifre Üretmek İçin Butona Tıklayınız.")
+btn = st.button("Şifre")
+if btn:
+    st.write(sifre)
+'''
+st.header('Kaynak Kodları')
+st.code(kod,language='python')
